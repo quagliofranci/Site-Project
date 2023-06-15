bb7608311b855ed41f8ccf2a2b1f67c71a171bd1
Kirito-Emo <emanuelerelmi@gmail.com>
2023-06-15 12:19:23 +0200

    new file:   homepage.php

---
+++ homepage.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Volontari per gli anziani</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href=""
+    </head>
+
+    <body>
+        <div class="container">
+            <?php include("./header.php"); ?>
