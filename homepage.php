5a0e8b1ad20795ca0aab16da0c40f5e6bdc3304c
Kirito-Emo <emanuelerelmi@gmail.com>
2023-06-19 21:17:11 +0200

    new file:   homepage.css new file:   homepage.php

---
+++ homepage.php
@@ -0,0 +1,73 @@
+<?php
+    session_start();
+    // include(../aggiorna_cookie.php);
+?>
+
+<!DOCTYPE html>
+<html lang="it">
+    <head>
+        <title>Sorrisi d'Argento</title>
+        <meta charset="utf-8">
+        <meta name="author" content="Gruppo 53"/>
+        <meta name="description" content="Un sito dedicato al volontariato verso gli anziani."/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <meta name="keywords" content="Volontariato anziani assistenza"/>
+        <link rel="icon" href="./images/icon.jpg" type="image/jpg"/>
+        <link rel="stylesheet" type="text/css" href="./homepage.css"/>
+    </head>
+
+    <body style="background-image:url(./images/homepage2.jpg); background-position: center; background-size: cover; background-attachment: fixed;">
+        <div class="container">
+            
+            <!-- HEADER -->
+            <?php include("./header-home.php"); ?>
+            
+            <!-- SEZIONE CENTRALE -->
+            <div class="upper">
+                <div class="column-upper-right">
+                    <h1>SIAMO LIETI DI AIUTARVI</h1>
+
+                    <p>I servizi che offriamo sono molteplici, spaziano dalla semplice chiacchierata alle commissioni fino ad aiuti in ambito sanitario</p>
+                    
+                    <button class="button1">
+                        <a href="./chi-siamo.php">SCOPRICI</a>
+                    </button>
+
+                    <button class="button2">
+                        <a href="./prenota.php">PRENOTA</a>
+                    </button>
+                </div>
+            </div>
+            
+            <div class="central">
+                <div class="column-central-right">
+                    <h1>AIUTACI A FARE LA DIFFERENZA!</h1>
+                    
+                    <button class="button3">
+                        <a href="./chi-siamo.php">UNISCITI!</a>
+                    </button>
+                </div>
+            </div>
+            
+            <!--
+            <div class="contatti">
+                <h1>Dove puoi contattarci?</h1>
+
+                <div class="riga">
+                    <div class="Centralino" id="centralino" style="width: 200px; height: 100px;">
+                        <p class="descrizione" id="descrizione1" style="display:block">Centralino</br>0420-69-90-104</p>
+                    </div>
+
+                    <div class="E-mail" id="e-mail" style="width: 200px; height: 100px;">
+                        <p class="descrizione" id="descrizione2" style="display:block">E-mail</br>assistenza-anziani@gmail.com</p>
+                    </div>
+
+                    <div class="Dove Siamo" id="dove_siamo" style="width: 200px; height: 100px;">
+                        <p class="descrizione" id="descrizione3" style="display:block">Dove Siamo</br>Via Roma, 208</p>
+                    </div>
+                </div>
+            </div>
+            -->
+        </div>
+    </body>
+</html>
