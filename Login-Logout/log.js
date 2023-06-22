7143ba1ecb404e58ef6e26b7e8018f57c6605b99
Kirito-Emo <emanuelerelmi@gmail.com>
2023-06-22 19:51:48 +0200

    modified:   Login-Logout/log.css new file:   Login-Logout/log.js modified:   Login-Logout/log.php

---
+++ Login-Logout/log.js
@@ -0,0 +1,47 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("accedi").addEventListener("click", function() {
+        var formContainer = document.querySelector(".form");
+        formContainer.style.transform = "rotateY(0deg)";
+    });
+
+    document.getElementById("registrati").addEventListener("click", function() {
+        var formContainer = document.querySelector(".form");
+        formContainer.style.transform = "rotateY(180deg)";
+    });
+});
+
+function validateForm() {
+
+    var nome = document.getElementById("reg-name").value;
+    var cognome = document.getElementById("reg-surname").value;
+    var email = document.getElementById("reg-email").value;
+    var password = document.getElementById("reg-pass").value;
+
+    if(nome.length < 2)
+    {
+        alert("Il campo 'Nome' deve contenere almeno 2 caratteri");
+        return false;
+    }
+
+    if(cognome.length < 3)
+    {
+        alert("Il campo 'Cognome' deve contenere almeno 3 caratteri");
+        return false;
+    }
+
+    function validateEmail(email) {
+        const emailPattern = /^[^\s@]+@[^\s@]+.[^\s@]+$/;
+        alert("L'email inserita non è valida");
+        return emailPattern.test(email);
+    }
+
+
+    if(password.length < 8)
+    {
+        alert("Il campo 'password' deve contenere almeno 8 caratteri");
+        return false;
+    }
+
+    return true;
+}
--- Login-Logout/log.php
+++ Login-Logout/log.php
@@ -5,16 +5,17 @@
 <!DOCTYPE html>
 <html lang="it">
     <head>
-        <title>Sorrisi d'Argento</title>
+        <title>Sorrisi d'Argento - Auth</title>
         <meta charset="utf-8">
         <meta name="author" content="Gruppo 53"/>
-        <meta name="description" content="Un sito dedicato al volontariato verso gli anziani."/>
+        <meta name="description" content="Pagina di autenticazione relativa al sito 'Sorrisi d'Argento'"/>
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
         <meta name="keywords" content="Volontariato anziani assistenza"/>
         <link rel="icon" href="../images/icon.jpg" type="image/jpg"/>
         <link rel="stylesheet" type="text/css" href="./log.css"/>
         <!-- link per scaricare i font necessari per le icone -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <script src="./log.js"></script>
     </head>
 
     <body>
@@ -25,45 +26,55 @@
             <!-- Form -->
             <div class="row">
 				<div class="column">
-                    <!-- opzioni per la checkbox -->
+                    <!-- pulsanti per switchare tra i form -->
 					<h6>
-                        <span id="accedi">Accedi</span>
-                        <span id="registrati">Registrati</span>
+                        <button class="login-btn" id="accedi">Accedi</button>
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <button class="reg-btn" id="registrati">Registrati</button>
                     </h6>
-			          	
-                    <!-- checkbox per switchare tra login e registrazione -->
-                    <input class="checkbox" type="checkbox" id="reg-log"/>
-                    <label for="reg-log"></label>
-						
-                    <!-- corpo del form -->
+					
 					<div class="form">
 						<div class="form-divider">
+                            <!-- corpo del form di login -->
 							<div class="login-form">
 								<div class="form-wrapper">
-									<h4>Accedi</h4></br></br>
+                                    <form id="AccediForm">
+                                        <div class="form-fieldset">
+                                            <input type="email" class="fieldset-style" id="log-email" placeholder="La tua email"></br>
+                                            <input type="password" class="fieldset-style" id="log-psw" placeholder="La tua password"></br></br></br>
+                                        </div>
 
-									<div class="form-fieldset">
-										<input type="email" class="fieldset-style" id="log-email" placeholder="La tua email"></br>
-										<input type="password" class="fieldset-style" id="log-psw" placeholder="La tua password"></br></br></br>
-									</div>
-
-									<a href="#" class="login-button">Accedi</a></br></br>
-
-                        			<p><a href="#" class="forgot-psw">Password dimenticata?</a></p>
-			      				</div>
+									    <input type="submit" class="login-button" name="AccediButton" value="Accedi"></br></br>
+                                    </form>
+                                </div>
 			      			</div>
 
+                            <!-- corpo del form di registrazione -->
 							<div class="register-form">
 								<div class="form-wrapper">
-									<h4>Registrati</h4></br>
+                                    <form id="RegisterForm" onsubmit="return validateForm();">
+                                        <div class="form-fieldset">
+                                            <input type="text" class="fieldset-style" id="reg-name" placeholder="Il tuo nome" required></br></br>
+                                            <input type="text" class="fieldset-style" id="reg-surname" placeholder="Il tuo cognome" required></br></br>
+                                            
+                                            <p style="font-size: bolder; font-weight: 500; margin-bottom: 15px;">Data di nascita</p>
+                                            <input type="date" class="fieldset-style" id="reg-date" required></br></br>
+                                            
+                                            <input type="radio" class="radio-btn" id="radiobtn" name="sex" style="vertical-align: middle" required/>
+                                            <label class="radio-btn" for="reg-male">M</label>
 
-									<div class="form-fieldset">
-										<input type="text" class="fieldset-style" id="log-name" placeholder="Il tuo nome"></br></br>
-										<input type="email" class="fieldset-style" id="log-email" placeholder="La tua email"></br>
-										<input type="password" class="fieldset-style" id="logpass" placeholder="La tua password"></br></br>
-									</div>
+                                            <input type="radio" class="radio-btn" id="radiobtn" name="sex" style="vertical-align: middle" required/>
+                                            <label class="radio-btn" for="reg-female">F</label>
 
-									<a href="#" class="register-button">Registrati</a>
+                                            <input type="radio" class="radio-btn" id="radiobtn" name="sex" style="vertical-align: middle" required/>
+                                            <label class="radio-btn" for="reg-neutral">Altro</label></br></br>
+                                            
+                                            <input type="email" class="fieldset-style" id="reg-email" placeholder="La tua email" required></br>
+                                            <input type="password" class="fieldset-style" id="reg-pass" placeholder="La tua password" required></br></br>
+                                        </div>
+
+									    <input type="submit" class="register-button" name="RegistratiButton" value="Registrati">
+                                    </form>
 				      			</div>
 			      			</div>
 			      		</div>
