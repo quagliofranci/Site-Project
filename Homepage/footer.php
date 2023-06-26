6f96168848da07682630d0bb0850fd05aaebd0c8
quagliofranci <quagliuolofranci@gmail.com>
2023-06-26 11:47:23 +0200

    modified:   Homepage/footer.php

--- Homepage/footer.php
+++ Homepage/footer.php
@@ -2,7 +2,7 @@
     <!-- link per scaricare i font necessari per le icone -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     
-    <h2>Puoi anche trovarci quo</h2>
+    <h2>Puoi anche trovarci qui</h2>
     
     <ul class="socials">
         <li><a href="https://www.facebook.com/" target="_blank" class="fa fa-facebook" style="color: white;"></a></li>
