449b6c7c678d81408b0ea09d78a8163d6675f912
Kirito-Emo <76470231+Kirito-Emo@users.noreply.github.com>
2023-06-30 14:05:05 +0200

    modified:   Chi-Siamo/chi-siamo.css; Homepage/header, homepage.css; Login-Logout/log.css; Prenota/prenota.css; Servizi/servizi.css

--- Homepage/header.php
+++ Homepage/header.php
@@ -1,11 +1,22 @@
 <header>
     <a class="logo">Sorrisi d'Argento</a>
 
-    <ul>
+    <ul id="menu">
         <li><a href="../Homepage/homepage.php">HOME</a></li>
         <li><a href="../Servizi/servizi.php">SERVIZI</a></li>
         <li><a href="../Chi-Siamo/chi-siamo.php" id="chi-siamo">CHI SIAMO</a></li>
         <li><a href="../Prenota/prenota.php">PRENOTA</a></li>
-        <li><a href="../Login-Logout/log.php">LOGIN</a></li>
+        <li><a href="../Login-Logout/log.php">LOGIN <i class="fa fa-caret-down"></i></a>
+            <ul>
+                <li><a href="../Login-Logout/log.php">PRENOTAZIONI</i></a></li>
+                <li><a href="../Homepage/homepage.php">ESCI</i></a></li>
+            </ul>
+        </li>
     </ul>
 </header>
+
+<script>
+    function showLoginAlert() {
+        alert("Effettua l'accesso per accedere a PRENOTA!");
+    }
+</script>
